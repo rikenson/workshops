0185c208a84211677e6976c5aa6f5b431c04884d
Rikenson JACQUES <rikensonjacques@gmail.com>
2023-06-08 17:39:10 -0400

    Integration de Junit5 et asserJ

--- workshop-mgt-rest/src/test/java/com/mendoz/workshop/AppTests.java
+++ workshop-mgt-rest/src/test/java/com/mendoz/workshop/AppTests.java
@@ -1,13 +1,23 @@
 package com.mendoz.workshop;
 
+import com.mendoz.workshop.payload.WorkshopResponse;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+
 class AppTests {
 
 	@Test
 	void contextLoads() {
+
+		//given
+		var underTest = new WorkshopResponse();
+		//when
+		underTest.setStatus(WorkshopResponse.StatusEnum.PENDING);
+		//then
+		assertThat(underTest).isNotNull();
 	}
 
 }
