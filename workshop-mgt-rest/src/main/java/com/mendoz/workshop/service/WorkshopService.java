b606d107fef62d2e0883d60c9dc3cfcaf3fa5ba5
Rikenson JACQUES <rikensonjacques@gmail.com>
2023-06-08 16:27:51 -0400

    mise en place de la couche controller

--- workshop-mgt-rest/src/main/java/com/mendoz/workshop/service/WorkshopService.java
+++ workshop-mgt-rest/src/main/java/com/mendoz/workshop/service/WorkshopService.java
@@ -1,8 +1,8 @@
 package com.mendoz.workshop.service;
 
 
-import com.mendoz.workshop.domain.entity.Participant;
 import com.mendoz.workshop.domain.filter.WorkshopFilter;
+import com.mendoz.workshop.payload.ParticipantRequest;
 import com.mendoz.workshop.payload.WorkshopRequest;
 import com.mendoz.workshop.payload.WorkshopResponse;
 
@@ -15,7 +15,7 @@ public interface WorkshopService {
 
     WorkshopResponse edit(WorkshopRequest request, UUID uuid);
 
-    WorkshopResponse addParticipants(List<Participant> participants, UUID uuid);
+    WorkshopResponse addParticipants(List<ParticipantRequest> participants, UUID uuid);
 
     WorkshopResponse retrieve(UUID uuid);
 
