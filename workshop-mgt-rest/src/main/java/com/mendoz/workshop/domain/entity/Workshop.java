c8ce8bcf1c63d013b73c957b6a19f8d8d378dbea
Rikenson JACQUES <rikensonjacques@gmail.com>
2023-06-08 17:10:41 -0400

    mise en place de la couche controller

--- workshop-mgt-rest/src/main/java/com/mendoz/workshop/domain/entity/Workshop.java
+++ workshop-mgt-rest/src/main/java/com/mendoz/workshop/domain/entity/Workshop.java
@@ -5,17 +5,12 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 @Entity
 @Table(name = "Team")
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor @SuperBuilder
@@ -24,7 +19,9 @@ public class Workshop extends BaseEntity {
     private String description;
     private LocalDateTime startDateTime;
     private LocalDateTime endDateTime;
+    @Builder.Default
     private WorkshopStatus status = WorkshopStatus.PENDING;
+    @Builder.Default
     @OneToMany(cascade = CascadeType.DETACH)
     private List<Participant> participants = new ArrayList<>();
 }
